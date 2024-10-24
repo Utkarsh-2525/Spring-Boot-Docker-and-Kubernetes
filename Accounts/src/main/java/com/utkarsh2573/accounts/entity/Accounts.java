91983329898f6899cf618c7aafe17a97a9e81359
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-10-24 21:54:50 +0530

    initial commit

---
+++ Accounts/src/main/java/com/utkarsh2573/accounts/entity/Accounts.java
@@ -0,0 +1,22 @@
+package com.utkarsh2573.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class Accounts extends BaseEntity {
+    @Column(name = "customer_id") // No need to mention if column name == field name
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
---
+++ Accounts/src/main/java/com/utkarsh2573/accounts/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.utkarsh2573.accounts.repository;
+
+import com.utkarsh2573.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
