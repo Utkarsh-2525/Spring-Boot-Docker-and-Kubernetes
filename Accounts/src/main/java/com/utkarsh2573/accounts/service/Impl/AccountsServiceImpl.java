9f69b4c21b0850a4941ab5e224307d0ebd8163ee
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-10-30 02:05:57 +0530

    Updated by, Created by, Updated at and Created at implemented

--- Accounts/src/main/java/com/utkarsh2573/accounts/service/Impl/AccountsServiceImpl.java
+++ Accounts/src/main/java/com/utkarsh2573/accounts/service/Impl/AccountsServiceImpl.java
@@ -36,9 +36,6 @@ public class AccountsServiceImpl implements IAccountsService {
 
         if (optionalCustomer.isPresent())
             throw new CustomerAlreadyExistsException("Customer already registered with mobile number " + customerDto.getMobileNumber());
-
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("M.Utkarsh");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -55,8 +52,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("M.Utkarsh");
         return newAccount;
     }
 
@@ -117,6 +112,4 @@ public class AccountsServiceImpl implements IAccountsService {
         customerRepository.deleteById(customer.getCustomerId());
         return true;
     }
-
-
 }
