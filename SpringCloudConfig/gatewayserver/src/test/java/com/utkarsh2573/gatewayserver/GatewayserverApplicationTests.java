c14446b4b774a5d843b3f52789872f73812dbe89
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-12-18 01:15:17 +0530

    Gateway Server created

---
+++ SpringCloudConfig/gatewayserver/src/test/java/com/utkarsh2573/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh2573.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
