e91736630f88a5e0b9df6cd7cf5f733f98729a6f
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-12-18 17:27:38 +0530

    Custom routes designed and implemented

--- SpringCloudConfig/gatewayserver/src/main/java/com/utkarsh2573/gatewayserver/GatewayserverApplication.java
+++ SpringCloudConfig/gatewayserver/src/main/java/com/utkarsh2573/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.utkarsh2573.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,24 @@ public class GatewayserverApplication {
         SpringApplication.run(GatewayserverApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(customPath -> customPath
+                        .path("/demobank/accounts/**")
+                                .filters(filter -> filter
+                                        .rewritePath("/demobank/accounts/(?<segment>.*)","/${segment}"))
+                                .uri("lb://ACCOUNTS"))
+                .route(customPath -> customPath
+                        .path("/demobank/loans/**")
+                        .filters(filter -> filter
+                                .rewritePath("/demobank/accounts/(?<segment>.*)","/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(customPath -> customPath
+                        .path("/demobank/cards/**")
+                        .filters(filter -> filter
+                                .rewritePath("/demobank/accounts/(?<segment>.*)","/${segment}"))
+                        .uri("lb://CARDS"))
+                .build();
+    }
 }
